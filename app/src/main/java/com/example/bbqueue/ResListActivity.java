b31ca9fc02c15639a346688e77af5b25f0d38185
Jim Lin <jhlin5083@gmail.com>
2020-10-01 15:32:52 -0700

    Hardcoded constraints ver

--- app/src/main/java/com/example/bbqueue/ResListActivity.java
+++ app/src/main/java/com/example/bbqueue/ResListActivity.java
@@ -1,21 +1,14 @@
 package com.example.bbqueue;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
-public class ResListActivity extends WearableActivity {
-
-    private TextView mTextView;
+public class ResListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_list);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
 }
