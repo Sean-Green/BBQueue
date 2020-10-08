355a2df144b8c2c13da67390e052c04fb21434f1
Johnny Scott <johnny>
2020-10-08 13:45:07 -0700

    timer function/test commit

--- app/src/main/java/com/example/bbqueue/MainActivity.java
+++ app/src/main/java/com/example/bbqueue/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bbqueue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(30000, 1000) {
+            TextView mTextField = findViewById(R.id.textView);
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mTextField.setText("done!");
+            }
+        }.start();
+
     }
 }
