2637cfba0271f06c29b85c0c63f29562c0887d2f
Johnny Scott <johnny>
2020-10-08 14:08:44 -0700

    fixed some childish mistakes -Johnny

--- app/src/main/java/com/example/bbqueue/InQueue.java
+++ app/src/main/java/com/example/bbqueue/InQueue.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class InQueue extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inqueue);
 
         new CountDownTimer(30000, 1000) {
             TextView mTextField = findViewById(R.id.textView);
