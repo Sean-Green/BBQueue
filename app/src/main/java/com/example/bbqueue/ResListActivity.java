62d1a3103485f9233001e2e1a93cadd38d92aa74
Jim Lin <jhlin5083@gmail.com>
2020-10-15 13:30:15 -0700

    ResList ActionBar back option

--- app/src/main/java/com/example/bbqueue/ResListActivity.java
+++ app/src/main/java/com/example/bbqueue/ResListActivity.java
@@ -1,9 +1,11 @@
 package com.example.bbqueue;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ResListActivity extends AppCompatActivity {
@@ -12,9 +14,20 @@ public class ResListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_list);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
     public void navQueue(View view) {
         Intent intent = new Intent(this, InQueue.class);
         startActivity(intent);
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id==android.R.id.home) {
+            finish();
+        }
+        return false;
     }
 }
