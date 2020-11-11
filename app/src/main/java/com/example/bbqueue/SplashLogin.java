8378cd42b14f5e39ada31dbbb7eebc160d5d2041
Johnny Scott <johnny>
2020-11-10 21:02:11 -0800

    Made er look nice

--- app/src/main/java/com/example/bbqueue/SplashLogin.java
+++ app/src/main/java/com/example/bbqueue/SplashLogin.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
