31303dcc954ac91187594bea8993eb3effd093e0
Johnny Scott <johnny>
2020-11-12 16:03:40 -0800

    login requires verification now

--- app/src/main/java/com/example/bbqueue/SplashLogin.java
+++ app/src/main/java/com/example/bbqueue/SplashLogin.java
@@ -8,11 +8,15 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +26,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,6 +40,10 @@ public class SplashLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_splash_login);
+        TextView warning = findViewById(R.id.textView10);
+        warning.setVisibility(View.GONE);
+        Button resend = findViewById(R.id.btnResendVeri);
+        resend.setVisibility(View.GONE);
     }
 
     public void login(View view) throws InterruptedException {
@@ -41,25 +51,24 @@ public class SplashLogin extends AppCompatActivity {
         String E = Email.getText().toString().trim();
         EditText Password = findViewById(R.id.LoginPassword);
         String P = Password.getText().toString().trim();
+
         signIn(E, P);
         TimeUnit.SECONDS.sleep(1);
-        getUserCust();
-        AccountTypeCheck();
     }
 
-    public void loginStore(View view) throws InterruptedException {
-        EditText Email = findViewById(R.id.LoginEmail);
-        String E = Email.getText().toString().trim();
-        EditText Password = findViewById(R.id.LoginPassword);
-        String P = Password.getText().toString().trim();
-        signIn(E, P);
-        TimeUnit.SECONDS.sleep(1);
-        getUserStore();
-        TimeUnit.SECONDS.sleep(1);
-        Intent intent = new Intent(this, Store_Activity.class);
-        intent.putExtra("mAuth", mAuth.getCurrentUser().getUid());
-        startActivity(intent);
-    }
+//    public void loginStore(View view) throws InterruptedException {
+//        EditText Email = findViewById(R.id.LoginEmail);
+//        String E = Email.getText().toString().trim();
+//        EditText Password = findViewById(R.id.LoginPassword);
+//        String P = Password.getText().toString().trim();
+//        signIn(E, P);
+//        TimeUnit.SECONDS.sleep(1);
+//        getUserStore();
+//        TimeUnit.SECONDS.sleep(1);
+//        Intent intent = new Intent(this, Store_Activity.class);
+//        intent.putExtra("mAuth", mAuth.getCurrentUser().getUid());
+//        startActivity(intent);
+//    }
 
     public void signIn(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
@@ -70,6 +79,15 @@ public class SplashLogin extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             //Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+
+                            if(mAuth.getCurrentUser().isEmailVerified() && mAuth != null){
+                                AccountTypeCheck();
+                            } else{
+                                TextView warning = findViewById(R.id.textView10);
+                                warning.setVisibility(View.VISIBLE);
+                                Button resend = findViewById(R.id.btnResendVeri);
+                                resend.setVisibility(View.VISIBLE);
+                            }
                             } else {
                             // If sign in fails, display a message to the user.
                             //Log.w(TAG, "signInWithEmail:failure", task.getException());
@@ -115,48 +133,63 @@ public class SplashLogin extends AppCompatActivity {
     }
 
 
-
-    private void getUserCust() {
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Users");
-        myRef = myRef.child(mAuth.getCurrentUser().getUid());
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+    public void sendVerification(View view){
+        mAuth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                Customer value = dataSnapshot.getValue(Customer.class);
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(SplashLogin.this, "Verification Sent", Toast.LENGTH_LONG).show();
             }
-
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SplashLogin.this, "Verification email not sent", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private void getUserStore() {
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Users");
-        myRef = myRef.child(mAuth.getCurrentUser().getUid());
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                Restaurant value = dataSnapshot.getValue(Restaurant.class);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-            }
-        });
-    }
+//    private void getUserCust() {
+//
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Users");
+//        myRef = myRef.child(mAuth.getCurrentUser().getUid());
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                Customer value = dataSnapshot.getValue(Customer.class);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//            }
+//        });
+//    }
+//
+//    private void getUserStore() {
+//
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Users");
+//        myRef = myRef.child(mAuth.getCurrentUser().getUid());
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                Restaurant value = dataSnapshot.getValue(Restaurant.class);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//            }
+//        });
+//    }
 
 
 
@@ -166,21 +199,21 @@ public class SplashLogin extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void navFrontOfQueue(View view) {
-        Intent intent = new Intent(this, Front_Queue.class);
-        startActivity(intent);
-    }
-
-    public void navResList(View view) {
-        Intent intent = new Intent(this, ResListActivity.class);
-        startActivity(intent);
-    }
-    public void navStoreFront(View view) {
-        Intent intent = new Intent(this, Store_Activity.class);
-        startActivity(intent);
-    }
-    public void navQueue(View view) {
-        Intent intent = new Intent(this, InQueue.class);
-        startActivity(intent);
-    }
+//    public void navFrontOfQueue(View view) {
+//        Intent intent = new Intent(this, Front_Queue.class);
+//        startActivity(intent);
+//    }
+//
+//    public void navResList(View view) {
+//        Intent intent = new Intent(this, ResListActivity.class);
+//        startActivity(intent);
+//    }
+//    public void navStoreFront(View view) {
+//        Intent intent = new Intent(this, Store_Activity.class);
+//        startActivity(intent);
+//    }
+//    public void navQueue(View view) {
+//        Intent intent = new Intent(this, InQueue.class);
+//        startActivity(intent);
+//    }
 }
